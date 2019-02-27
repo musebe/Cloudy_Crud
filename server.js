59a920a503d970c9aea0419cc376b3a91dd5afc0
musebe <emusebe9@gmail.com>
2019-02-27 11:07:40 +0000

    Finalized delete route

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
-const hbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const upload = require("./handlers/multer");
-const mongoose = require("mongoose");
 const cloudinary = require("cloudinary");
 
 require("dotenv").config();
@@ -12,14 +10,33 @@ require("./handlers/cloudinary");
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.engine("handlebars", hbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.set("view engine", "ejs");
 
 // @route GET /
 // @desc Loads form
-app.get("/", (req, res) => {
-  res.render("index");
+
+app.get("/", async (req, res) => {
+  const images = await cloudinary.v2.api.resources({
+    type: "upload",
+    prefix: "image"
+  });
+  // Check if files
+  if (!images || images.length === 0) {
+    res.render("index", { images: false });
+  } else {
+    images.resources.map(image => {
+      if (
+        image.format === "jpg" ||
+        image.format === "png" ||
+        image.format === "jpeg"
+      ) {
+        image.isImage = true;
+      } else {
+        image.isImage = false;
+      }
+    });
+    res.render("index", { images });
+  }
 });
 
 // @route POST /upload
@@ -54,9 +71,29 @@ app.get("/files", async (req, res) => {
 // @desc  Display single file object
 app.get("/files/:id", async (req, res) => {
   const id = req.params.id;
-  console.log(id);
-  cloudinary.v2.api.resources_by_ids(`Image/{id}`, function(error, result) {
-    console.log(result);
+  await cloudinary.v2.api.resources_by_ids(`Image/${id}`, (err, image) => {
+    // Check if file
+    if (!image || image.length === 0) {
+      return res.status(404).json({
+        err: "No image exists"
+      });
+    }
+    // File exists
+    return res.json(image);
+    //console.log(result);
+  });
+});
+
+// @route DELETE /files/:id
+// @desc  Delete file
+app.delete("/files/:id", (req, res) => {
+  const id = req.params.id;
+  cloudinary.v2.uploader.destroy(`Image/${id}`, (err, result) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+
+    res.redirect("/");
   });
 });
 
